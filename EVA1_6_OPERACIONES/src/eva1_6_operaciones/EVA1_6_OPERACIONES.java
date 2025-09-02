2c2ffc9de822347799542bdec1d9a1d422327bf4
JavierPerez2203 <L25550656@chihuahua2.tecnm.mx>
2025-09-02 09:06:09 -0700

    Add files via upload

---
+++ EVA1_6_OPERACIONES/src/eva1_6_operaciones/EVA1_6_OPERACIONES.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_operaciones;
+
+/**
+ *
+ * @author javier
+ */
+public class EVA1_6_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double AREA;
+        double p;
+        double RADIO;
+        //ES LO MISMO QUE:
+        //DOUBLE PI;
+        //DOUBLE RADIO;
+        //DOUBLE AREA;
+        //INICIALIZACION:
+        p = 3.1416;
+        RADIO = 10;
+        //OPERACION
+        AREA = p * RADIO * RADIO;
+        //SALIDA DE DATOS:
+        System.out.println("EL AREA DEL CIRCULO DE RADIO = 10 ES ");
+        System.out.println(AREA);
+    }
+    
+}
---
+++ EVA1_7_AREA_TRIANGULO/src/eva1_7_area_triangulo/EVA1_7_AREA_TRIANGULO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_area_triangulo;
+
+/**
+ *
+ * @author javier
+ */
+public class EVA1_7_AREA_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double area;
+        double base;
+        double altura;
+        
+        base = 5;
+        altura = 18;
+        
+        area =base * altura / 2;
+        
+        System.out.println("el area de un triangulo es");
+        System.out.println(area);
+                
+    }
+    
+}
